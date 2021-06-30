d14f9b78c789a4756fb394b2a027e62c630296f2
meli-jobrito <joao.brito@mercadolivre.com>
2021-06-29 23:29:39 -0300

    Alteracao em ex. 2 da Aula 2 TT

--- src/aula2tt/Exercicio2/Util.java
+++ src/aula2tt/Exercicio2/Util.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Util {
     public static double areaMedia(ArrayList<GeometricFigure> l){
-        double areaTotal = 0;
-        for(GeometricFigure g : l){
-            areaTotal += g.area();
-        }
+        double areaTotal = l.stream().mapToDouble(GeometricFigure::area).sum();//uau
+        //        for(GeometricFigure g : l){
+//            areaTotal += g.area();
+//        }
         return areaTotal / l.size();
     }
 }
